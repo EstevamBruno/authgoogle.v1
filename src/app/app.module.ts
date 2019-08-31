113f0d358120c89f1afcb069de907985a939cf66
Bruno Kennedy <brunokennedy16@gmail.com>
2019-08-31 17:20:27 -0300

    auth google login Angular v8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,48 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginModule } from './login/login.module';
+
+// Import fontAwesome
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+// Modulos para inicio de sessão com redes sociais google/facebook
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+
+// Variável de autenticação
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("773409356933-8uq06jg302tt656m8ofq46oumq13bcdk.apps.googleusercontent.com")
+  },
+  // {
+  //   id: FacebookLoginProvider.PROVIDER_ID,
+  //   provider: new FacebookLoginProvider("Facebook-App-Id")
+  // }
+]);
+ 
+export function provideConfig() {
+  return config;
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    SocialLoginModule,
+    AngularFontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
